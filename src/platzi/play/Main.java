f5365fd53ec56588e408a2204bec78f6691c9aa1
Eduardo Martínez <soka_117@live.com>
2026-07-15 18:30:58 -0600

    feat: se ocupa la clase scanner para interactuar con la consola

--- src/platzi/play/Main.java
+++ src/platzi/play/Main.java
@@ -1,8 +1,20 @@
 package platzi.play;
 
+import java.util.Scanner;
+
 public class Main {
-    // public static void main(String[] args) { // we do not use public because of Java version 25; in earlier versions it is used
     static void main(String[] args) {
-        System.out.println("Hello Word");
+        System.out.println("Platzi Play");
+
+        Scanner scanner = new Scanner(System.in); // with this parameter we will be reviewing what the user enters via the console
+        System.out.println("¿Cuál es tu nombre?");
+        String name = scanner.nextLine(); // this is a variable, we capture the next line that the user types into the console
+
+        System.out.println("Hola " + name + ", esto es Platzi Play 🚀");
+
+        System.out.println(name + "¿Cuanto años tienes?");
+        int age = scanner.nextInt();
+
+        System.out.println(name + ", puedes ver contenido +" + age);
     }
 }
