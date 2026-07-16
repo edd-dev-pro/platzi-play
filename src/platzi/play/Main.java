bf037bfaff839c0065e7d36b10fab499adcb281f
Eduardo Martínez <soka_117@live.com>
2026-07-15 22:53:25 -0600

    feat: se crea un clase utilitaria usando static para compartir metodos

--- src/platzi/play/Main.java
+++ src/platzi/play/Main.java
@@ -2,38 +2,32 @@ package platzi.play;
 
 import platzi.play.contenido.Movie;
 import platzi.play.plataforma.User;
+import platzi.play.util.ScannerUtils;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Main {
     static void main(String[] args) {
-        System.out.println("Platzi Play 🍿");
+        System.out.println("Platzi Play");
+
+        String title = ScannerUtils.getText("Nombre del contenido");
+        String movieGenre = ScannerUtils.getText("Género del contenido");
+        int duration = ScannerUtils.getNumber("Duración del contenido");
+        double rating = ScannerUtils.getDecimal("Calificación del contenido");
 
         Movie movie = new Movie();
-
-        movie.title = "El Señor de los Anillos";
+        movie.title = title;
         movie.releaseDate = LocalDate.of(2018, 10, 15);
-        movie.movieGenre = "Fantasía";
-        movie.rateMovie(4.7);
-        movie.duration = 120;
+        movie.movieGenre = movieGenre;
+        movie.duration = duration;
+        movie.rateMovie(rating);
 
-        long durationLong = movie.duration; // any whole number fits in a long string
-        int ratingInt = (int) movie.rating; // (int) it refers to the data type of the destination
-        // long numberOfPrizes = Long.parseLong("25");
-        int numberOfPrizes = (int) Long.parseLong("25000000000"); // this prints a negative number, since an int type doesn't support such large numbers
-
-        System.out.println("Duración Long: " + durationLong);
-        System.out.println("Calificación Int: " + ratingInt);
-        System.out.println("Numero de premios: " + numberOfPrizes);
+        System.out.println(movie.getTechnicalSpecifications());
 
         User user = new User();
-
         user.name = "Eduardo";
         user.registrationDate = LocalDateTime.of(2026, 7, 15, 19, 24, 12);
-
-        System.out.println(user.registrationDate);
-
         user.watchMovie(movie);
     }
 }
