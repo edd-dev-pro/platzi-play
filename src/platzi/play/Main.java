5d4018eac17e04a1ce7390eeb1d6760ff885ba30
Eduardo Martínez <soka_117@live.com>
2026-07-15 23:07:34 -0600

    feat: creamos constantes con ayuda de final para tener valores inmutables

--- src/platzi/play/Main.java
+++ src/platzi/play/Main.java
@@ -8,8 +8,11 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Main {
+    public static final String PLATFORM_NAME = "Platzi Play";
+    public static final String VERSION = "1.0.0";
+
     static void main(String[] args) {
-        System.out.println("Platzi Play");
+        System.out.println(PLATFORM_NAME + " - " + VERSION);
 
         String title = ScannerUtils.getText("Nombre del contenido");
         String movieGenre = ScannerUtils.getText("Género del contenido");
