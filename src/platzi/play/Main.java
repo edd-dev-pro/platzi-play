fb7cb02bf6453e91449c12ed5375a152d6858339
Eduardo Martínez <soka_117@live.com>
2026-07-18 15:36:10 -0600

    feat: se crea la clase plataforma para utilizar una lista que almacene las peliculas de la plataforma

--- src/platzi/play/Main.java
+++ src/platzi/play/Main.java
@@ -1,6 +1,7 @@
 package platzi.play;
 
 import platzi.play.contenido.Movie;
+import platzi.play.plataforma.Platform;
 import platzi.play.plataforma.User;
 import platzi.play.util.ScannerUtils;
 
@@ -9,6 +10,8 @@ public class Main {
     public static final String VERSION = "1.0.0";
 
     static void main(String[] args) {
+        Platform platform = new Platform(PLATFORM_NAME);
+
         System.out.println(PLATFORM_NAME + " - " + VERSION);
 
         String title = ScannerUtils.getText("Nombre del contenido");
@@ -17,8 +20,15 @@ public class Main {
         double rating = ScannerUtils.getDecimal("Calificación del contenido");
 
         Movie movie = new Movie(title, duration, movieGenre, rating);
+        Movie movie_two = new Movie("El origen", duration, movieGenre, rating);
 
-        System.out.println(movie.getTechnicalSpecifications());
+        platform.addMovie(movie);
+        platform.addMovie(movie_two);
+
+        System.out.println("Numero de elementos en la platafoma: " + platform.getContent().size());
+
+        platform.removeMovie(movie_two);
+        platform.showTitles();
 
         User user = new User("Eduardo", "eduardo@gmail.com");
         user.watchMovie(movie);
---
+++ src/platzi/play/plataforma/Platform.java
@@ -0,0 +1,38 @@
+package platzi.play.plataforma;
+
+import platzi.play.contenido.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Platform {
+    private String name;
+    private List<Movie> content;
+
+    public Platform(String name) {
+        this.name = name;
+        this.content = new ArrayList<>();
+    }
+
+    public void addMovie(Movie movie) {
+        this.content.add(movie);
+    }
+
+    public void showTitles() {
+        for (Movie movie : content) {
+            System.out.println(movie.getTitle());
+        }
+    }
+
+    public void removeMovie(Movie movie) {
+        this.content.remove(movie);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Movie> getContent() {
+        return content;
+    }
+}
